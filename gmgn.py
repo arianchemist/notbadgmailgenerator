a5dc47051372f13d49caaa3aab82dd89d3d45ff2
arianchemist <74075426+arianchemist@users.noreply.github.com>
2021-06-28 20:17:34 +0430

    gmail generator

---
+++ gmgn.py
@@ -0,0 +1,129 @@
+import random
+
+print("{++}")
+print("{++}")
+print("{++}")
+print("completely made by arianchemist")
+print("{++}")
+print("{++}")
+print("{++}")
+print()
+print()
+print()
+print()
+print()
+o = ["a" , "b" , "c" , "d" , "e" , "f" , "g" , "h" , "i" , "j" , "k" , "l" , "m" , "n" , "o" , "p" , "q" , "r" , "s" , "t" , "u" , "v" , "w" , "x" , "y" , "z"]
+
+oo = ["1" , "2" , "3" , "4" , "5" , "6" , "7" , "8" , "9" , "0"]
+
+orr = ["1" , "0"]
+t = random.choice(o)
+t2 = random.choice(o)
+t3 = random.choice(o)
+t4 = random.choice(o)
+t5 = random.choice(o)
+t6 = random.choice(o)
+t7 = random.choice(o)
+t8 = random.choice(o)
+
+d = random.choice(oo)
+d2 = random.choice(oo)
+d3 = random.choice(oo)
+d4 = random.choice(oo)
+
+oorr = random.choice(orr)
+oorr2 = random.choice(orr)
+
+print(t + t2 + t3 + t4 + t5 + t6 + t7 + t8 + d + d2 + d3 + d4 + "@gmail.com")
+print("this is better")
+print("""
+   |
+   |
+   |
+  \ /""")
+print(t + t2 + t3 + t4 + t5 + t6 + t7 + t8 + "199" + d + d2 + "gmail.com")
+print(t + t2 + t3 + t4 + t5 + t6 + t7 + t8 + "19" + d + d2 + d3 + "gmail.com")
+print(t + t2 + t3 + t4 + t5 + t6 + t7 + t8 + "202" + oorr + "gmail.com")
+print(t + t2 + t3 + t4 + t5 + t6 + t7 + t8 + "20" + oorr + oorr2 +"gmail.com")
+
+
+print("4 different emails")
+print("""
+   |
+   |
+   |
+  \ /""")
+
+h = ["a" , "b" , "c" , "d" , "e" , "f" , "g" , "h" , "i" , "j" , "k" , "l" , "m" , "n" , "o" , "p" , "q" , "r" , "s" , "t" , "u" , "v" , "w" , "x" , "y" , "z"]
+
+ood = ["1" , "2" , "3" , "4" , "5" , "6" , "7" , "8" , "9" , "0"]
+
+orrf = ["1" , "0"]
+q = random.choice(h)
+q2 = random.choice(h)
+q3 = random.choice(h)
+q4 = random.choice(h)
+q5 = random.choice(h)
+q6 = random.choice(h)
+q7 = random.choice(h)
+q8 = random.choice(h)
+
+l = random.choice(ood)
+l2 = random.choice(ood)
+l3 = random.choice(ood)
+l4 = random.choice(ood)
+
+oorrf = random.choice(orrf)
+oorr2f = random.choice(orrf)
+print(q + q2 + q3 + q4 + q5 + q6 + q7 + q8 + "199" + l + l2 + "gmail.com")
+print(q + q2 + q3 + q4 + q5 + q6 + q7 + q8 + "19" + l + l2 + l3 + "gmail.com")
+print(q + q2 + q3 + q4 + q5 + q6 + q7 + q8 + "202" + oorrf + "gmail.com")
+print(q + q2 + q3 + q4 + q5 + q6 + q7 + q8 + "20" + oorrf + oorr2f +"gmail.com")
+
+
+print("4 different emails")
+print("""
+   |
+   |
+   |
+  \ /""")
+
+
+
+
+noiir = 1
+
+for noiir in range(1 , 5):
+    ow = ["a" , "b" , "c" , "d" , "e" , "f" , "g" , "h" , "i" , "j" , "k" , "l" , "m" , "n" , "o" , "p" , "q" , "r" , "s" , "t" , "u" , "v" , "w" , "x" , "y" , "z"]
+
+    ooww = ["1" , "2" , "3" , "4" , "5" , "6" , "7" , "8" , "9" , "0"]
+
+    orrwww = ["1" , "0"]
+    z = random.choice(ow)
+    z2 = random.choice(ow)
+    z3 = random.choice(ow)
+    z4 = random.choice(ow)
+    z5 = random.choice(ow)
+    z6 = random.choice(ow)
+    z7 = random.choice(ow)
+    z8 = random.choice(ow)
+
+    j = random.choice(ooww)
+    j2 = random.choice(ooww)
+    j3 = random.choice(ooww)
+    j4 = random.choice(ooww)
+
+    oorr = random.choice(orrwww)
+    oorr2 = random.choice(orrwww)
+    print(z + z2 + z3 + z4 + z5 + z6 + z7 + z8 + j + j2 + j3 + j4 + "@gmail.com")
+
+
+
+
+
+
+
+
+
+
+#completely made by arianchemist
